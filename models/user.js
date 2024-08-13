488afa2afacd6e7937cd2a63423ad3778fccc1f9
Khanhhuynh <khanhhuynh3508@gmail.com>
2024-08-13 22:28:45 +0700

    fix user model bugs

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
-const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const UserSchema = new Schema({
@@ -10,6 +9,10 @@ const UserSchema = new Schema({
         unique: [true, 'That email address has already been used']
     }
 })
-
+UserSchema.plugin(passportLocalMongoose,{
+    
+    //   selectFields : 'email username password' //Space seperate the required fields
+    
+});
 module.exports = mongoose.model('User', UserSchema)
 
